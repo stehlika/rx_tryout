a2e84f56ff53ac588eca97d7a3aaa46f588e0603
LIborStark <libor.stark@interautomation.de>
2019-12-07 18:33:06 +0100

    Polling

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var observable = Rx.Observable.create((observer) => {
-    observer.next()
+let myObservable = Rx.Observable.interval(1000).concatMap(()=>{
+    return Rx.Observable.fromPromise(
+        $.get('http://localhost:8080/warnings')
+    )
+        .catch((response) => {
+            return Rx.Observable.empty();
+        })
 });
 
-let userTypesInSearchBox = Rx.Observable.fromEvent(
+myObservable.subscribe(results =>{
+    console.log("refreshed");
+    renderUsers(results.warnings)
+});
+
+/*let userTypesInSearchBox = Rx.Observable.fromEvent(
     $("#search-box"),
     'keyup'
 )
     .map((event) => {
         return $("#search-box").val();
     });
+*/
 
-
-
-Rx.Observable.create((observer) => {
+/*Rx.Observable.create((observer) => {
     observer.map((event) => {
         event
     .debounce(250)
@@ -25,36 +34,37 @@ Rx.Observable.create((observer) => {
                     });
             });
     })
-});
+});*/
 
 
 
 
-let searchResult = observable
+/*let searchResult = observable
     .debounce(250)
     .concatMap(() => {
-        return Rx.Observable.fromPromise(
+        return Rx.Observable.interval(1000).fromPromise(
             $.get('http://localhost:8080/warnings')
         )
             .catch((response) => {
                 return Rx.Observable.empty();
-            });
+            })
     });
-
-Rx.Observable
+*/
+/*Rx.Observable
     .combineLatest(searchResult)
     .subscribe((results) => {
         renderUsers(
             results[0].warnings
         );
     });
-
+*/
 function renderUsers(
     result
 ) {
-    for(var i in result){
+    $("#output").empty();
+    for(var i in result) {
         $("#output").append(`<li>
-id=${result[i].id}`+`, severity = ${result[i].severity}
+id=${result[i].id}` + `, severity = ${result[i].severity}` + ` type = ${result[i].prediction.type}
 </li>`)
     }
     //$("#output").append("<li> hello </li>")
