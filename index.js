53e58e3f0305363f46bb4f9a6f1cbddf9f81eb5c
AdamStehlik <stehlikadam@gmail.com>
2019-12-08 20:06:13 +0100

    for now functioning

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myObservable = Rx.Observable.interval(1000).concatMap(()=>{
+let myObservable = Rx.Observable.interval(1000).concatMap(() => {
     return Rx.Observable.fromPromise(
         $.get('http://localhost:8080/warnings')
     )
@@ -7,19 +7,92 @@ let myObservable = Rx.Observable.interval(1000).concatMap(()=>{
         })
 });
 
-myObservable.subscribe(results =>{
-    console.log("refreshed");
-    renderUsers(results.warnings)
+var _results = null;
+var _resultsFiltered = [];
+var _resultsNewer = [];
+
+let daco = myObservable.subscribe(results => {
+    if (_results == null) {
+        _results = results.warnings
+    } else {
+        let allIds = _results.map(x => x.id);
+        for (element of results.warnings) {
+            if (allIds.includes(element.id)) {
+                //console.log("This is already contained : "+element.id)
+                for (obj of _results) {
+                    if (obj.id == element.id) {
+                        if (obj.severity != element.severity) {
+                            // Pridat do noveho listu
+                            _resultsNewer.push(element)
+                        }
+                    }
+                }
+            } else {
+                //console.log("This could be added : "+element.id)
+                _results.push(element)
+            }
+        }
+        /*for (ele of results.warnings) {
+            for (element of _results) {
+                if(element === ele) {
+                    console.log("Nic sa neudej")
+                } else {
+                    console.log("malo by sa pridat ")
+                    _results.push(ele)
+                }
+            }
+            /*if (_results.includes(ele)) {
+                console.log("Uz to tam je")
+            } else {
+                _results.push(ele)
+            }*/
+    }
+    renderWarnings(_results)
+    //renderNewResults(_resultsNewer) // TODO
 });
 
-/*let userTypesInSearchBox = Rx.Observable.fromEvent(
+let userCLickedDeactivate = Rx.Observable.fromEvent($("#deactivation"), 'click').subscribe(() => {
+    daco.dispose()
+});
+
+let userClickedActivate = Rx.Observable.fromEvent($("#activation"), 'click').subscribe(() => {
+    daco = myObservable.subscribe(results => {
+        _results = results;
+        console.log("refreshed");
+        renderWarnings(results.warnings)
+    })
+});
+
+
+let userTypesInSearchBox = Rx.Observable.fromEvent(
     $("#search-box"),
     'keyup'
 )
     .map((event) => {
+        daco.dispose();
+        if ($("#search-box").val() != "") {
+            _resultsFiltered = [];
+            for (var ele of _results.warnings) {
+                if (ele.severity <= $("#search-box").val()) {
+                    console.log("Not valid for condition" + ele.severity);
+                } else {
+                    console.log("valid for condition" + ele.severity);
+                    _resultsFiltered.push(ele)
+                }
+            }
+            renderWarnings(_resultsFiltered)
+        } else {
+            daco = myObservable.subscribe(results => {
+                _results = results;
+                console.log("refreshed");
+                renderWarnings(results.warnings)
+            });
+        }
+
         return $("#search-box").val();
     });
-*/
+
+userTypesInSearchBox.subscribe();
 
 /*Rx.Observable.create((observer) => {
     observer.map((event) => {
@@ -50,6 +123,7 @@ myObservable.subscribe(results =>{
             })
     });
 */
+
 /*Rx.Observable
     .combineLatest(searchResult)
     .subscribe((results) => {
@@ -58,11 +132,19 @@ myObservable.subscribe(results =>{
         );
     });
 */
-function renderUsers(
-    result
-) {
+function renderNewResults(result) {
+    $("#new_output").empty();
+    for (res of result) {
+        $("#new_output").append(`<li>
+id=${res.id}` + `, severity = ${res.severity}` + ` type = ${res.prediction.type}
+</li>`)
+    }
+}
+
+
+function renderWarnings(result) {
     $("#output").empty();
-    for(var i in result) {
+    for (var i in result) {
         $("#output").append(`<li>
 id=${result[i].id}` + `, severity = ${result[i].severity}` + ` type = ${result[i].prediction.type}
 </li>`)
---
+++ indexws.js
@@ -0,0 +1,41 @@
+let ws;
+
+let firstRun = true;
+let _resultsws = [];
+
+window.subscribe = () => {
+
+    ws = new WebSocket("ws://localhost:8090/warnings");
+
+    ws.onopen = () => {
+        const message = 'subscribe';
+        ws.send(message)
+    };
+
+    ws.onmessage = (message) => {
+        const data = JSON.parse(message.data);
+        console.log(JSON.stringify(data));
+        if (firstRun) {
+            _results = [];
+            firstRun = false;
+            _resultsws = data.warnings
+        } else {
+            _resultsws.push(JSON.parse(message.data))
+        }
+        renderNewResultsws(_resultsws);
+    }
+};
+
+window.subscribe();
+
+function renderNewResultsws(result) {
+    $("#output_ws").empty();
+    for (res of result) {
+        $("#output_ws").append(`<li>id=${res["id"]}` + `, severity = ${res["severity"]}` + `</li>`)
+    }
+}
+
+window.unsubscribe = () => {
+    firstRun = true;
+    ws.close();
+};
